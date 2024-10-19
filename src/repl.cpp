b38f37f776774de7a7a52606a46f923f0f05de2c
Jan Górski <jan.a.gorski@pw.edu.pl>
2024-10-19 13:45:05 +0200

    fixed multiple finished / not finished statements in one line

--- src/repl.cpp
+++ src/repl.cpp
@@ -1,28 +1,29 @@
 #include "../inc/repl.hpp"
 
 using namespace std;
-
 namespace repl {
-
 void kaczka(){};
 void start(istream& is, ostream& os) {
     string statement;
-    char input;
-    while (1) {
-        os << PROMPT;
-        do {
-            input = is.get();
-            statement += input;
-            if (input == '\n')
-                os << " | ";
-        }
-        while (input != ';');
+    string line;
 
-        input = is.get();
-        statement += input;
-        std::cout << statement << endl;
-        statement.clear();
+    while (true) {
+        os << PROMPT;
+        while (std::getline(is, line)) {
+            for (char ch: line) {
+                statement += ch;
+                if (ch == ';') {
+                    os << statement << endl;
+                    statement.clear();
+                } 
+            } 
+            if (line.back() == ';') {
+                os << PROMPT;
+                continue;
+            }
+            os << " | ";
+            line.clear();
+            }
+        }
     }
 }
-
-} // namespace repl
