c459d553e85cfbdf17afcc6cb4898be2ddc4d8e7
jgorski <01178321@pw.edu.pl>
2024-11-14 14:14:12 +0100

    added trivial tests

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -2,10 +2,71 @@
 #include <boost/test/unit_test.hpp>
 #include "../inc/parser.hpp"
 #include <cassert>
+#include <map>
 using namespace std;
 using namespace token;
 using namespace lexer;
 using namespace parser;
+
+const map<string, string> trivialCases {
+    {
+"1",
+"(IntegerLiteral:\n\
+    value=1)"
+    },
+    {
+"1+2",
+"(AdditiveExpression:\n\
+    left=(IntegerLiteral:\n\
+        value=1),\n\
+    operator=[+],\n\
+    right=(IntegerLiteral:\n\
+        value=2))"
+    },
+    {
+"100-100",
+"(AdditiveExpression:\n\
+    left=(IntegerLiteral:\n\
+        value=100),\n\
+    operator=[-],\n\
+    right=(IntegerLiteral:\n\
+        value=100))"
+    },
+    {
+"9*123",
+"(MultiplicativeExpression:\n\
+    left=(IntegerLiteral:\n\
+        value=9),\n\
+    operator=[*],\n\
+    right=(IntegerLiteral:\n\
+        value=123))"
+    },
+    {
+"9/123",
+"(MultiplicativeExpression:\n\
+    left=(IntegerLiteral:\n\
+        value=9),\n\
+    operator=[/],\n\
+    right=(IntegerLiteral:\n\
+        value=123))"
+    },
+};
+
+void testTrivial(const string& input, const string& expected) {
+    istringstream in(input);
+    Parser p(in);
+    unique_ptr<Node> output = p.generateParsingTree();
+    TreeGenVisitor visitor;
+    output->accept(visitor);
+    string received = visitor.getResult();
+    BOOST_CHECK_EQUAL(expected, received);
+}
+
+BOOST_AUTO_TEST_CASE(testTrivialCases) {
+    for (const auto& [i, e] : trivialCases) {
+        testTrivial(i, e);
+    }
+}
 
 BOOST_AUTO_TEST_CASE(testIntegerAddition) {
     string input = R"(1 + 2 * 3)";
