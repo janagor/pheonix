d96ac8b1ceca1317fe847cbfa7698bf729a919e1
jgorski <01178321@pw.edu.pl>
2024-11-22 14:44:41 +0100

    refactored tests

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -10,202 +10,199 @@ using namespace parser;
 const map<string, string> TRIVIAL_CASES {
     //IntegerLiteral
     {
-"1",
-"(IntegerLiteral:\n\
-    value=1)"
+"1;",
+"(ExpressionStatement:\n\
+    expression=(IntegerLiteral:\n\
+        value=1))"
     },
-    //TypeSpecifier
-    /*
-    {
-"str",
-"(TypeSpecifier:\n\
-    value=str)"
-    },
-    {
-"int",
-"(TypeSpecifier:\n\
-    value=int)"
-    },
-    {
-"flt",
-"(TypeSpecifier:\n\
-    value=flt)"
-    },
-    {
-"bol",
-"(TypeSpecifier:\n\
-    value=bol)"
-    },
-    */
     // CastExpression
     {
-"1<-int",
-"(CastExpression:\n\
-    expression=(IntegerLiteral:\n\
-        value=1),\n\
-    type=(TypeSpecifier:\n\
-        value=int))"
+"1<-int;",
+"(ExpressionStatement:\n\
+    expression=(CastExpression:\n\
+        expression=(IntegerLiteral:\n\
+            value=1),\n\
+        type=(TypeSpecifier:\n\
+            value=int)))"
     },
     {
-"1<-str",
-"(CastExpression:\n\
-    expression=(IntegerLiteral:\n\
-        value=1),\n\
-    type=(TypeSpecifier:\n\
-        value=str))"
+"1<-str;",
+"(ExpressionStatement:\n\
+    expression=(CastExpression:\n\
+        expression=(IntegerLiteral:\n\
+            value=1),\n\
+        type=(TypeSpecifier:\n\
+            value=str)))"
     },
     {
-"1<-flt",
-"(CastExpression:\n\
-    expression=(IntegerLiteral:\n\
-        value=1),\n\
-    type=(TypeSpecifier:\n\
-        value=flt))"
+"1<-flt;",
+"(ExpressionStatement:\n\
+    expression=(CastExpression:\n\
+        expression=(IntegerLiteral:\n\
+            value=1),\n\
+        type=(TypeSpecifier:\n\
+            value=flt)))"
     },
     {
-"1<-bol",
-"(CastExpression:\n\
-    expression=(IntegerLiteral:\n\
-        value=1),\n\
-    type=(TypeSpecifier:\n\
-        value=bol))"
+"1<-bol;",
+"(ExpressionStatement:\n\
+    expression=(CastExpression:\n\
+        expression=(IntegerLiteral:\n\
+            value=1),\n\
+        type=(TypeSpecifier:\n\
+            value=bol)))"
     },
     // PrefixExpression
     {
-"-2",
-"(PrefixExpression:\n\
-    operator=[-],\n\
-    expression=(IntegerLiteral:\n\
-        value=2))"
+"-2;",
+"(ExpressionStatement:\n\
+    expression=(PrefixExpression:\n\
+        operator=[-],\n\
+        expression=(IntegerLiteral:\n\
+            value=2)))"
     },
     {
-"!2",
-"(PrefixExpression:\n\
-    operator=[!],\n\
-    expression=(IntegerLiteral:\n\
-        value=2))"
+"!2;",
+"(ExpressionStatement:\n\
+    expression=(PrefixExpression:\n\
+        operator=[!],\n\
+        expression=(IntegerLiteral:\n\
+            value=2)))"
     },
     // AdditiveExpression
     {
-"1+2",
-"(AdditiveExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=1),\n\
-    operator=[+],\n\
-    right=(IntegerLiteral:\n\
-        value=2))"
+"1+2;",
+"(ExpressionStatement:\n\
+    expression=(AdditiveExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=1),\n\
+        operator=[+],\n\
+        right=(IntegerLiteral:\n\
+            value=2)))"
     },
     {
-"100-100",
-"(AdditiveExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=100),\n\
-    operator=[-],\n\
-    right=(IntegerLiteral:\n\
-        value=100))"
+"100-100;",
+"(ExpressionStatement:\n\
+    expression=(AdditiveExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=100),\n\
+        operator=[-],\n\
+        right=(IntegerLiteral:\n\
+            value=100)))"
     },
     // MultipliveExpression
     {
-"9*123",
-"(MultiplicativeExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[*],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9*123;",
+"(ExpressionStatement:\n\
+    expression=(MultiplicativeExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[*],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     {
-"9/123",
-"(MultiplicativeExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[/],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9/123;",
+"(ExpressionStatement:\n\
+    expression=(MultiplicativeExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[/],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     // RelationalExpression
     {
-"9<123",
-"(RelationalExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[<],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9<123;",
+"(ExpressionStatement:\n\
+    expression=(RelationalExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[<],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     {
-"9<=123",
-"(RelationalExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[<=],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9<=123;",
+"(ExpressionStatement:\n\
+    expression=(RelationalExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[<=],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     {
-"9>123",
-"(RelationalExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[>],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9>123;",
+"(ExpressionStatement:\n\
+    expression=(RelationalExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[>],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     {
-"9>=123",
-"(RelationalExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[>=],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9>=123;",
+"(ExpressionStatement:\n\
+    expression=(RelationalExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[>=],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     // ComparisonExpression
     {
-"9==123",
-"(ComparisonExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[==],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9==123;",
+"(ExpressionStatement:\n\
+    expression=(ComparisonExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[==],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     {
-"9!=123",
-"(ComparisonExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[!=],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9!=123;",
+"(ExpressionStatement:\n\
+    expression=(ComparisonExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[!=],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     // AndExpression
     {
-"9&&123",
-"(AndExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[&&],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9&&123;",
+"(ExpressionStatement:\n\
+    expression=(AndExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[&&],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     // OrExpression
     {
-"9||123",
-"(OrExpression:\n\
-    left=(IntegerLiteral:\n\
-        value=9),\n\
-    operator=[||],\n\
-    right=(IntegerLiteral:\n\
-        value=123))"
+"9||123;",
+"(ExpressionStatement:\n\
+    expression=(OrExpression:\n\
+        left=(IntegerLiteral:\n\
+            value=9),\n\
+        operator=[||],\n\
+        right=(IntegerLiteral:\n\
+            value=123)))"
     },
     // AssignementExpression
     {
-"$a=123",
-"(AssignementExpression:\n\
-    identifier=a,\n\
-    expression=(IntegerLiteral:\n\
-        value=123))"
+"$a=123;",
+"(ExpressionStatement:\n\
+    expression=(AssignementExpression:\n\
+        identifier=a,\n\
+        expression=(IntegerLiteral:\n\
+            value=123)))"
     },
     // ExpressionStatement
     {
