d6198f6442473a9a23f5107661644d3dc485afcc
jgorski <01178321@pw.edu.pl>
2024-12-10 02:54:41 +0100

    added basic usage of program

---
+++ app/pheonix.cpp
@@ -0,0 +1,77 @@
+#include "lexer.hpp"
+#include "parser.hpp"
+#include "visitor.hpp"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+std::vector<pheonix::lexer::Lexem> lexerize(pheonix::lexer::Lexer &lexer) {
+  std::vector<pheonix::lexer::Lexem> result;
+  while (true) {
+    pheonix::lexer::Lexem l = lexer.nextLexem();
+    result.emplace_back(l);
+    if (l.token.getTokenType() == pheonix::token::TokenType::END_OF_FILE) {
+      return result;
+    }
+  }
+}
+
+void help() {
+  std::cout << "Usage: ./pheonix [OPTIONS]" << std::endl << std::endl;
+  std::cout << "    -h           Display this message" << std::endl;
+  std::cout << "    -p INPUT     Generate parser output" << std::endl;
+  std::cout << "    -l INPUT     Generate lexer output" << std::endl;
+}
+
+int main(int argc, char **argv) {
+  if (argc <= 1) {
+    help();
+    return 0;
+  }
+
+  if (argc == 2 && std::string(argv[1]) == "-h") {
+    help();
+    return 0;
+  }
+
+  if (argc == 3 && std::string(argv[1]) == "-l") {
+    std::string filename = std::string(argv[2]);
+    std::ifstream input(filename);
+    if (!input) {
+      std::cerr << "Error: Could not open file.\n";
+      return 1; // Exit with error
+    }
+
+    pheonix::lexer::Lexer l(input);
+    std::vector<pheonix::lexer::Lexem> result = lexerize(l);
+    for (const auto &lexem : result) {
+      std::cout << lexem << std::endl;
+    }
+    std::cout << std::endl;
+    input.close();
+    return 0;
+  }
+
+  if (argc == 3 && std::string(argv[1]) == "-p") {
+    std::string filename = std::string(argv[2]);
+    std::ifstream input(filename);
+    if (!input) {
+      std::cerr << "Error: Could not open file.\n";
+      return 1;
+    }
+    pheonix::parser::Parser p(input);
+
+    std::unique_ptr<pheonix::node::Node> output = p.generateParsingTree();
+    pheonix::visitor::TreeGenVisitor visitor;
+    output->accept(visitor);
+    std::string received = visitor.getResult();
+
+    std::cout << received << std::endl;
+    input.close();
+    return 0;
+  }
+  std::cout << "Wrong params." << std::endl;
+  help();
+  return 1;
+}
