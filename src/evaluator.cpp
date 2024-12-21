5b356530365066276fbf481209c22e8899b8bf69
jgorski <01178321@pw.edu.pl>
2024-12-21 19:00:53 +0100

    evaluating block, while loop, if statement, and parent expresison

--- src/evaluator.cpp
+++ src/evaluator.cpp
@@ -20,15 +20,54 @@ void Evaluator::visit(node::Parameter &p) { UNUSED(p); }
 
 void Evaluator::visit(node::DeclarationArguments &da) { UNUSED(da); }
 
-void Evaluator::visit(node::Block &b) { UNUSED(b); }
+void Evaluator::visit(node::Block &b) {
+  for (size_t i = 0; i < b.statements.size(); ++i) {
+    b.statements[i]->accept(*this);
+  }
+}
 
 void Evaluator::visit(node::FunctionDeclaration &fd) { UNUSED(fd); }
 
-void Evaluator::visit(node::WhileLoopStatement &wls) { UNUSED(wls); }
+void Evaluator::visit(node::WhileLoopStatement &wls) {
+  while (true) {
+    wls.expression->accept(*this);
+    if (auto *predicate = std::get_if<bool>(&result)) {
+      if (predicate) {
+        wls.statements->accept(*this);
+        result = std::monostate();
+        continue;
+      }
+      result = std::monostate();
+      return;
+    }
+    // TODO: throw here because of a wrong type of expression
+    result = std::monostate();
+    return;
+  }
+}
 
 void Evaluator::visit(node::VariableDeclaration &vd) { UNUSED(vd); }
 
-void Evaluator::visit(node::IfStatement &is) { UNUSED(is); }
+void Evaluator::visit(node::IfStatement &is) {
+  is.predicate->accept(*this);
+  if (auto *predicate = std::get_if<bool>(&result)) {
+    if (predicate) {
+      is.ifBody->accept(*this);
+      result = std::monostate();
+      return;
+    }
+    if (is.ifBody) {
+      is.ifBody->accept(*this);
+      result = std::monostate();
+      return;
+    }
+
+    result = std::monostate();
+  }
+  // TODO: throw if predicate is not of bool type
+
+  result = std::monostate();
+}
 
 void Evaluator::visit(node::ReturnStatement &rs) { UNUSED(rs); }
 
@@ -124,7 +163,9 @@ void Evaluator::visit(node::Identifier &i) {
   result = context.context.at(i.value);
 }
 
-void Evaluator::visit(node::ParentExpression &pe) { UNUSED(pe); }
+void Evaluator::visit(node::ParentExpression &pe) {
+  pe.expression->accept(*this);
+}
 
 void Evaluator::visit(node::Literal &l) { result = l.value; }
 
