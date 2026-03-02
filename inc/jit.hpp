3c096c7860d633fdd415d81fdc9b1d89afb19c62
Jan Aleksander Górski <jan.a.gorski@wp.pl>
2026-03-02 14:27:17 +0900

    Added base for llvm jit extention.

---
+++ inc/jit.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include "context.hpp"
+#include "inst_visitor.hpp"
+#include "node.hpp"
+#include "object.hpp"
+#include "operator_visitor.hpp"
+#include "types.hpp"
+#include "visitor.hpp"
+
+#include <iostream>
+#include <llvm/ExecutionEngine/Orc/LLJIT.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/Module.h>
+#include <llvm/Support/TargetSelect.h>
+#include <stdexcept>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace pheonix {
+inline void init() {
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+}
+
+struct JIT : InstVisitor<JIT> {
+public:
+  JIT()
+      : m_result(0),
+        m_jit(llvm::ExitOnError()(llvm::orc::LLJITBuilder().create())),
+        m_context(std::make_unique<llvm::LLVMContext>()),
+        m_module(std::make_unique<llvm::Module>("module", *m_context)),
+        m_builder(*m_context) {}
+  void visitLiteral(pheonix::node::Literal &) {}
+  void visitAdditiveExpression(pheonix::node::AdditiveExpression &) {
+
+    llvm::FunctionType *FT = llvm::FunctionType::get(
+        m_builder.getInt32Ty(),
+        {m_builder.getInt32Ty(), m_builder.getInt32Ty()}, false);
+    llvm::Function *F = llvm::Function::Create(
+        FT, llvm::Function::ExternalLinkage, "add", m_module.get());
+
+    llvm::BasicBlock *BB = llvm::BasicBlock::Create(*m_context, "entry", F);
+    m_builder.SetInsertPoint(BB);
+    llvm::Value *Sum = m_builder.CreateAdd(F->arg_begin(), F->arg_begin() + 1);
+    m_builder.CreateRet(Sum);
+
+    llvm::ExitOnError()(m_jit->addIRModule(llvm::orc::ThreadSafeModule(
+        std::move(m_module), std::move(m_context))));
+
+    auto AddSym = llvm::ExitOnError()(m_jit->lookup("add"));
+    auto *AddFn = AddSym.toPtr<int (*)(int, int)>();
+
+    int wynik = AddFn(10, 32);
+    std::cout << "Wynik JIT (10 + 32): " << wynik << std::endl;
+  }
+  [[nodiscard]] int const &result() const noexcept { return m_result; }
+
+private:
+  int m_result;
+  std::unique_ptr<llvm::orc::LLJIT> m_jit;
+  std::unique_ptr<llvm::LLVMContext> m_context;
+  std::unique_ptr<llvm::Module> m_module;
+  llvm::IRBuilder<> m_builder;
+};
+
+} // namespace pheonix
---
+++ tests/test_jit.cpp
@@ -0,0 +1,25 @@
+#include <istream>
+#include <memory>
+
+#include "jit.hpp"
+#include "node.hpp"
+#include "types.hpp"
+
+void test2() {
+  pheonix::init();
+
+  pheonix::node::Literal a{pheonix::Primitive(pheonix::types::Integer{1})};
+  pheonix::node::Literal b{pheonix::Primitive(pheonix::types::Integer{2})};
+  auto expression = pheonix::node::AdditiveExpression{
+      std::make_unique<pheonix::node::Literal>(a),
+      std::make_unique<pheonix::node::Literal>(b), "+"};
+  pheonix::JIT jit{};
+  jit.visitAdditiveExpression(expression);
+}
+int main() {
+  test2();
+  // pheonix::node::Literal numNode{
+  //     pheonix::Primitive(pheonix::types::Integer{0})};
+  // pheonix::JIT jit{};
+  // jit.visitLiteral(numNode);
+}
